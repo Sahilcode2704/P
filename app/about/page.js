bd6346017bc7b7ce8d900bb83e913bc99f3ba0ca
Sahilcode2704 <sahiln27042008@gmail.com>
2025-09-19 14:42:39 +0530

    update

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main style={{ fontFamily: 'Arial, sans-serif', padding: '2rem' }}>
+      <h1>About Us</h1>
+      <p>We are building awesome stuff with Next.js 13.</p>
+      <a href="/">Home</a>
+    </main>
+  );
+}
